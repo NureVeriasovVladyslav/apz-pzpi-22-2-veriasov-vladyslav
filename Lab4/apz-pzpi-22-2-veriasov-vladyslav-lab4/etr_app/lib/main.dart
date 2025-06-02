e932b4f76c238a520751437fefe830713060da87
vladveriasov <vladverwolf5@gmail.com>
2025-06-02 11:21:44 +0200

    integration mapbox + flutter works and shows map

---
+++ Lab4/apz-pzpi-22-2-veriasov-vladyslav-lab4/etr_app/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+// Pass your access token to MapboxOptions so you can load a map
+  String ACCESS_TOKEN = const String.fromEnvironment("ACCESS_TOKEN");
+  // console.log("Access Token: $ACCESS_TOKEN");
+  // String ACCESS_TOKEN = "pk.eyJ1IjoidmxhZHZlciIsImEiOiJjbWJkZGtkbmkwNTduMmpzMjg4M2JjYjF5In0.2XOJ48oBSQeytEgL-I4erQ"
+  MapboxOptions.setAccessToken(ACCESS_TOKEN);
+
+  // Define options for your camera
+  CameraOptions camera = CameraOptions(
+    center: Point(coordinates: Position(-98.0, 39.5)),
+    zoom: 2,
+    bearing: 0,
+    pitch: 0);
+
+  // Run your application, passing your CameraOptions to the MapWidget
+  runApp(MaterialApp(home: MapWidget(
+    cameraOptions: camera,
+  )));
+}
